aeffd7a8f4996a0e554773dc60d5408bfb9bd0d9
anthonychemaly <anthonychemalygr@gmail.com>
2021-05-31 17:04:13 +0300

    auth

--- routes/article.js
+++ routes/article.js
@@ -4,11 +4,11 @@ var apis = require("../controllers/article.controller");
 var auth = require("../middlewares/auth");
 
 /* GET users listing. */
-router.get("/my", auth("author", "admin"), apis.getArticleByHost);
+router.get("/my", auth(["author", "admin"]), apis.getArticleByHost);
 router.get("/:id", apis.getArticle);
-router.put("/:id", auth("author", "admin"), apis.updateArticle);
-router.delete("/:id", auth("author", "admin"), apis.deleteArticle);
+router.put("/:id", auth(["author", "admin"]), apis.updateArticle);
+router.delete("/:id", auth(["author", "admin"]), apis.deleteArticle);
 router.get("/", apis.getAllArticles);
-router.post("/", auth("author", "admin"), apis.postArticle);
+router.post("/", auth(["author", "admin"]), apis.postArticle);
 
 module.exports = router;
